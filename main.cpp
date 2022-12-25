cc7af96115e8d3f62770c00a68999a80298dca57
IvanTIP <tipivanp@gmail.com>
2022-12-25 16:53:52 +0300

    release v1.0

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cpr/cpr.h>
+
+int main() {
+    cpr::Response r = cpr::Get (cpr::Url("http://httpbin.org/html"),
+                                cpr::Header({{"accept", "text/html"}}));
+    for (int i = r.text.find("<h1>") + 4;i < r.text.find("</h1>");i++) {
+        std::cout << r.text[i];
+    }
+    return 0;
+}
